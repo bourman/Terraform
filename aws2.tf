6bc193aade563961114b9dae563fb3fd39def061
Viktor <victorbourman77@gmail.com>
2023-08-31 00:57:33 +0300

    main

---
+++ aws2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = "aws_access_key_victor"
+  secret_key = "12344paswor123z"
+  region     = "eu-central-1"
+
+}
+
+resource "aws_instance" "My_Ubuntu" {
+  count         = "3"
+  ami           = "ami-03f38e546e3dc59e1"
+  instance_type = "t2.micro"
+
+}
