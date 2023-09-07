44dd92c3994f32412c1ac69b913299541c033972
Viktor <victorbourman77@gmail.com>
2023-09-07 11:18:04 +0300

    main

---
+++ project_10/README.md
@@ -0,0 +1,3 @@
+for example we dont have access to aws/azure cloud 
+and we need get the information about the Ip Address or AMI or security group 
+so we gona use output command to get it 
---
+++ project_10/use_output_command.tf
@@ -0,0 +1,67 @@
+#--------------------------------------
+# My Terraform                        -
+# Build WebServer during Bootsrap     -
+# Made by Victor Bourman              -
+#--------------------------------------
+provider "aws" {
+  region = "eu-east-2"
+}
+resource "aws_eip" "My_static_ip" {
+  instance = aws_instance.My_webserver.id
+  #now it will first up the new server then down the previous server and now We will shorten our DOWNTIME
+  #it will take the ip of the firest server and attached to the new server
+
+}
+
+resource "aws_instance" "My_webserver" {
+  ami                    = "ami-0371cec707bfc3d7" #Amazon Linux AMI
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.My_webserver.id]
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Victor"
+    l_name = "Bourman"
+    names  = ["Vasya", "Kolya", "Petya", "john", "Donald", "Masha", "new_name"]
+  })
+  tags = {
+    Name  = "Web Server Build by Terraform"
+    Owner = "Victor Bourman"
+  }
+  lifecycle {
+    create_before_destroy = true
+
+  }
+}
+#first my Security Group will created, then the resource aws_instance will be created.
+resource "aws_security_group" "My_webserver" {
+  name        = "WebServer Security Group"
+  description = "My first SecurityGroup"
+
+  ingress = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 433
+      to_port     = 433
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name  = "Web Server SecurityGroup"
+    Owner = "Victor Bourman"
+  }
+}
+output "webserver_instance_id" {
+  value = aws_instance.My_webserver.id
+
+}
